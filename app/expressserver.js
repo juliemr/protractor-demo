211850fb68ad1c64a3258e59b09d16ce5a4431b8
Julie Ralph <ju.ralph@gmail.com>
2015-06-29 16:22:49 -0700

    chore: util.puts -> console.log

--- app/expressserver.js
+++ app/expressserver.js
@@ -4,4 +4,4 @@ var util = require('util');
 
 app.use(express.static(__dirname));
 app.listen(3456);
-util.puts('Server running at http://localhost:3456');
+console.log('Server running at http://localhost:3456');
